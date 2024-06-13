1cc3d66c58a2f14c8b22c99281692f87e810ab68
Egor <atlas9509@gmail.com>
2024-06-13 14:23:17 +0300

    completed task
    Data_Types
    Variables_var_let_const

---
+++ JS Core Advanced/Data_Types_4.js
@@ -0,0 +1,6 @@
+function Data_Types(number){
+    var str = String(number)
+    console.log(str.length)
+}
+
+console.log(Data_Types(123456789))
---
+++ JS Core Advanced/Variables_var_let_const_4.js
@@ -0,0 +1,10 @@
+function variables(){
+    var a = "Test"
+    let b = 1
+    const c = 1.1
+    console.log(a, b ,c)
+}
+
+variables()
+
+console.log(a, b ,c) // вне функции данные переменные не доступны
